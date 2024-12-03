ea67ffd0c0a9688894a60c5437f7238198069824
Lahari Banukunta <152053025+LahariBanukunta@users.noreply.github.com>
2024-12-03 19:34:46 +0530

    Update EventHandling.js

--- eventHandling/EventHandling.js
+++ eventHandling/EventHandling.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function EventHandling() {
   const [count, setCount] = useState(0);
-  
+
   const countIncrement = () => {
     setCount(count + 1);
   };
@@ -12,41 +12,26 @@ function EventHandling() {
     justifyContent: 'center',
     alignItems: 'center',
     minHeight: '100vh',
-    backgroundColor: '#8bc6ec',
   };
 
   const EventDivStyle = {
     textAlign: 'center',
     padding: '20px',
-    backgroundColor: 'lavender',
     width: '300px',
     height: '300px',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     borderRadius: '10px',
+    border: '1px solid black',
   };
 
   const buttonStyle = {
     padding: '10px 20px',
-    
-    color: '#fff',
-    backgroundColor: '#333333',
-    border: 'none',
+    border: '1px solid black',
     borderRadius: '5px',
     cursor: 'pointer',
-  
     margin: '10px 5px', // Adds space between buttons
-  };
-
-  const incrementButtonStyle = {
-    ...buttonStyle,
-    backgroundColor: '#4CAF50', // Green color for increment
-  };
-
-  const resetButtonStyle = {
-    ...buttonStyle,
-    backgroundColor: '#FF5252', // Red color for reset
   };
 
   return (
@@ -56,17 +41,13 @@ function EventHandling() {
         <h2>Count is: {count}</h2>
         <button
           onClick={countIncrement}
-          style={incrementButtonStyle}
-          onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = '#45a049')} // Darker green on hover
-          onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = incrementButtonStyle.backgroundColor)}
+          style={buttonStyle}
         >
           Increment
         </button>
         <button
           onClick={() => setCount(0)}
-          style={resetButtonStyle}
-          onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = '#e57373')} // Lighter red on hover
-          onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = resetButtonStyle.backgroundColor)}
+          style={buttonStyle}
         >
           Reset
         </button>
