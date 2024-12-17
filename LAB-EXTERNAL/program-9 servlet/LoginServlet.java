2fc88d8c653a1324cc4c677c50296377b999f851
Lahari Banukunta <152053025+LahariBanukunta@users.noreply.github.com>
2024-12-17 21:17:03 +0530

    Update LoginServlet.java

--- LAB-EXTERNAL/program-9 servlet/LoginServlet.java
+++ LAB-EXTERNAL/program-9 servlet/LoginServlet.java
@@ -1,6 +1,7 @@
 package com.example;
 import java.util.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,6 +25,7 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        PrintWriter out = response.getWriter();
 
         if (username.equals(validUsername) && password.equals(validPassword)) {
             HttpSession session = request.getSession();
@@ -31,10 +33,10 @@ public class LoginServlet extends HttpServlet {
             session.setAttribute("loginTime", new Date());
             response.sendRedirect("welcome");
         } else {
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("<h3>Invalid username or password.</h3>");
-            response.getWriter().println("<a href='index.html'>Try Again</a>");
-            response.getWriter().println("</body></html>");
+            out.println("<html><body>");
+            out.println("<h3>Invalid username or password.</h3>");
+            out.println("<a href='index.html'>Try Again</a>");
+            out.println("</body></html>");
         }
     }
 }
